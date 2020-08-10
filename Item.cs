d5b018009e941305de7fbab11ada7bcbbaa8564f
praiseisaac <daramolapraise@gmail.com>
2020-08-10 15:37:52 -0500

    first commit

---
+++ Item.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Data;
+using System.Threading.Tasks;
+using MySqlConnector;
+using System.Collections.Generic;
+
+namespace BuyStuffApi
+{
+    public class Item {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int quantity{ get; set; }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Data;
+using System.Threading.Tasks;
+using MySqlConnector;
+using System.Collections.Generic;
+
+namespace BuyStuffApi
+{
+    public class Order {
+        public int Id { get; set; }
+        public List<Item> items { get; set; }
+        public double total_cost { get; set; }
+        public DateTime date_created { get; set; }
+        public DateTime date_shipped { get; set; }
+        public string tracking_number { get; set; }
+        public string delivery_address { get; set; }
+        public double shipping_cost { get; set; }
+        public Status status { get; set; }
+        public DateTime date_delivered { get; set; }
+    }
+}
